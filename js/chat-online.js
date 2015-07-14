900d73fc6ee6ba7dbb13ea67df31f852c8317367
verticalrollermill <posuiman@gmal.com>
2015-07-14 16:58:20 +0800

    first commit

---
+++ js/chat-online.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+document.writeln("<script language=\"javascript\" src=\"http://pkt.zoosnet.net/JS/LsJS.aspx?siteid=PKT56764896&float=1&lng=en\"></script>");
+
+function Online_Chat(){
+	window.open('http://pkt.zoosnet.net/LR/Chatpre.aspx?id=PKT56764896&lng=en'+escape(window.location), '_blank', 'height=400, width=600, top=200, left=200, toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, status=no');
+}
